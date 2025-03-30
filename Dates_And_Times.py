2c02e95ec11b996fc134d62eb7c620149a3749ec
ZaneWallace20 <138349317+ZaneWallace20@users.noreply.github.com>
2025-03-30 15:16:32 -0500

    Added a class for finding times

---
+++ Dates_And_Times.py
@@ -0,0 +1,117 @@
+import calendar
+from datetime import datetime, timedelta
+import json
+import os
+from dateutil.relativedelta import relativedelta
+
+
+class Dates_Times():
+
+    # in months
+    max_future = 0
+
+    DATA_FOLDER = "data"
+    
+    config_dict = {}
+
+    days = {}
+
+    def _day_to_num(self, day):
+        days = {
+            "Monday": 0,
+            "Tuesday": 1,
+            "Wednesday": 2,
+            "Thursday": 3,
+            "Friday": 4,
+            "Saturday": 5,
+            "Sunday": 6
+        }
+        
+        return days[day]
+
+    def _fill_time_dict(self, start, end, time_array=None, delta=timedelta(minutes=30)):
+        if time_array is None:
+            time_array = []
+        
+        if start >= end:
+            return time_array
+        
+        new_start = start + delta
+
+        time_array.append((start, new_start))
+
+        return self._fill_time_dict(start = new_start, end = end, time_array = time_array, delta = delta)
+
+    def __init__(self):
+
+        config_path = os.path.join(self.DATA_FOLDER,"config.json")
+
+        with open(config_path, "r") as file:
+            self.config_dict = json.load(file)
+
+        self.days = self.config_dict["days"]
+        self.max_future = self.config_dict["maxFuture"]
+
+    def get_available(self, month = datetime.now().month, year = datetime.now().year):
+        
+        now = datetime.now()
+
+        current_date = datetime(now.year, now.month, 1)
+
+        future_date = datetime(year, month, 1)
+        
+        if current_date + relativedelta(months=self.max_future) < future_date:
+            return {}
+
+        full_month = calendar.monthcalendar(year, month)
+
+        nums_of_week = [self._day_to_num(i["day"]) for i in self.days]
+
+        available = []
+
+        for week in full_month:
+            for day_num, day in enumerate(week):
+
+                if day_num not in nums_of_week:
+                    continue
+                if day == 0:
+                    continue
+                
+                day_of_week = self.days[nums_of_week.index(day_num)]
+                
+                start = day_of_week["startTime"]
+
+                end = day_of_week["endTime"]
+                
+                temp_dict = {
+                    "weekday": day_of_week["day"],
+                    "month": month,
+                    "year": year,
+                    "day":day,
+                    "availableTimes":  [] 
+                }
+
+                temp_dict["availableTimes"] = self._fill_time_dict(
+                    datetime.strptime(start, "%I:%M %p"),  
+                    datetime.strptime(end, "%I:%M %p"))
+
+                available.append(temp_dict)
+        return available
+
+                
+x = Dates_Times()
+for i in x.get_available():
+    print(i["weekday"],i["month"],i["day"],i["year"])
+    for start_time, end_time in i["availableTimes"]:
+        print(f"{" "*10}{start_time.strftime('%I:%M %p')} - {end_time.strftime('%I:%M %p')}")
+
+
+
+            
+
+
+
+
+
+        
+
